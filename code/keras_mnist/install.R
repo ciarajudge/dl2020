6de2175cf9cfebee4a89e7b039faca36f1ea7236
Stephen J. Eglen <sje30@cam.ac.uk>
2020-11-19 10:57:35 +0000

    note how to install keras

--- code/keras_mnist/install.R
+++ code/keras_mnist/install.R
@@ -1,5 +1,10 @@
 
+## Step 0: check what your quota is on linux: "quota"
+
+## 
 ## Step 1:
+## install keras; you will be asked if you wish to install miniconda; say yes.
+
 install.packages("keras")
 
 
@@ -16,3 +21,4 @@ system.time( keras::install_keras() )
 system.time ( rmarkdown::render('mnist_bp.Rmd') )
 
 
+## Step 4: repeat step 0 to check you are not over quota.  
